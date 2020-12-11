0daffb343a9e55e1a9d045f6d2002c7abe68e0ca
Abigail Limpioso <abby@Abigails-MacBook-Pro.local>
2020-12-11 02:04:41 -0500

    added function demonstrating one way to use a switch

---
+++ CalculateGrade.js
@@ -0,0 +1,24 @@
+function getGrade(score) {
+    let grade;
+    // returns the grade depending on score
+    switch(true) {
+        case (score > 25):
+            grade = 'A';
+            break;
+        case (score > 20):
+            grade = 'B';
+            break;
+        case (score > 15):
+            grade = 'C';
+            break;
+        case (score > 10):
+            grade = 'D';
+            break;
+        case (score > 5):
+            grade = 'E';
+            break;
+        case (score <= 5):
+            grade = 'F';
+    }
+    return grade;
+}
